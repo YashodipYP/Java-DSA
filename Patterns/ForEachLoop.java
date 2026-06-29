966ca0bf9857872e63846ae34486d345fd554bad
YashodipYP <yashodippatil78@gmail.com>
2026-06-29 10:00:26 +0530

    Delete Patterns/ForEachLoop.java

--- Patterns/ForEachLoop.java
+++
@@ -1,8 +0,0 @@
-public class ForEachLoop {
-    public static void main(String[] args) {
-        int[] arr = {5,8,2,6,9};
-        for(int ele: arr){
-            System.out.println(ele);
-        }
-    }
-}
