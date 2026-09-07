f05bb8c974ac3e2a963be050714ee2bf3e17054f
Your Real Name <yashodippatil78@gmail.com>
2026-09-07 10:26:31 +0530

    Added Codes

--- Linked_List/src/DisplayList.java
+++ Linked_List/src/DisplayList.java
@@ -5,7 +5,15 @@ class Node{
         this.val = val;
     }
 }
-public class NodeOfLinkedList {
+public class DisplayList {
+    public static void display(Node head){
+         Node temp = head;
+         while (temp != null){
+             System.out.println(temp.val+" ");
+             temp = temp.next;//very imp
+         }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Node a = new Node(10);
         Node b = new Node(20);
@@ -13,21 +21,11 @@ public class NodeOfLinkedList {
         Node d = new Node(40);
         Node e = new Node(50);
 
-        a.val = 10;
-        b.val = 10;
-        c.val = 10;
-        d.val = 10;
-        e.val = 10;
         //Connect Karna ( linking )
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
-
-        System.out.println(a.next.next.next.val);
-        System.out.println(c);
-        System.out.println(b.next);
-        System.out.println(a.next.next);
-
+        display(a);
     }
 }
